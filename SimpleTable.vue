345f4ec0981f7e8de388c3bf58555c61e6da3159
LeoYu <chaoyuleo@gmail.com>
2016-08-17 09:49:58 +0800

    更新分页功能

--- SimpleTable.vue
+++ SimpleTable.vue
@@ -18,15 +18,33 @@
           <span aria-hidden="true">&laquo;</span>
         </a>
       </li>
-      <li :class="{'active': $index == currentPage}" v-for="n in pageNum">
-        <a href="javascript:void(0);" @click="changePage($index)">
-          {{$index + 1}}
+      <li :class="{'active': n-1 == currentPage}" v-show="currentPage > 7">
+        <a href="javascript:void(0);" @click="changePage(0)">
+          1
+          <span class="sr-only">(current)</span>
+        </a>
+      </li>
+      <li :class="{'active': n-1 == currentPage}" v-for="n in tabPageNum">
+        <a href="javascript:void(0);" @click="changePage(n-1)">
+          {{n}}
+          <span class="sr-only">(current)</span>
+        </a>
+      </li>
+      <li :class="{'active': n-1 == currentPage}" v-show="currentPage < pageNum - 5">
+        <a href="javascript:void(0);" @click="changePage(pageNum - 1)">
+          {{pageNum}}
           <span class="sr-only">(current)</span>
         </a>
       </li>
       <li>
         <a href="javascript:void(0);" aria-label="Next" @click="preOrNext(1)">
           <span aria-hidden="true">&raquo;</span>
+        </a>
+      </li>
+      <li>
+        <a href="javascript:void(0);">
+          共{{ pageNum }}页
+          <span aria-hidden="true"></span>
         </a>
       </li>
     </ul>
@@ -65,6 +83,30 @@
     computed: {
       pageNum () {
         return Math.ceil(this.data.length / this.record)
+      },
+      tabPageNum () {
+        let left = 1
+        let right = this.pageNum
+        let ar = []
+        if (this.pageNum >= 11) {
+          if (this.currentPage > 7 && this.currentPage < this.pageNum - 5) {
+            left = this.currentPage - 4
+            right = this.currentPage + 2
+          } else {
+            if (this.currentPage <= 7) {
+              left = 1
+              right = 9
+            } else {
+              right = this.pageNum
+              left = this.pageNum - 7
+            }
+          }
+        }
+        while (left <= right) {
+          ar.push(left)
+          left++
+        }
+        return ar
       }
     },
     methods: {
